27ffeeebe4ef20d78072afec046671d27f102f09
RossUnruh <rossu@ksu.edu>
2024-09-25 17:58:13 -0500

    Update CoordinateCalculator.py
    Ross Unruh's Exercise 4 assignment.

--- CoordinateCalculator.py
+++ CoordinateCalculator.py
@@ -1,6 +1,23 @@
 ##-------------------------------------------------------------------------------------
 ## Name:  CoordinateCalculator.py
 ## Description:  Converts geographic coordinates in DDMMSS format to DD
-## Created:  June 19, 2023
-## Author:  Shawn Hutchinson (shutch@ksu.edu)
+## Created:  September 4th, 2024
+## Author:  Ross Unruh (rossu@ksu.edu)
+## Python Version: 3.12.0
 ##-------------------------------------------------------------------------------------
+
+# Request user-suppliesd coordinates in DDMMSS format:
+x = input('Please enter a coordinate value in DDMMSS format:    ')
+
+#Convert seconds to minutes
+# The negative 2 takes the last two inputs from the user.
+ss = int(x[-2:]) / 60
+
+# Add converted seconds to minutes and convert to degrees.
+mm = (int(x[-4:-2:]) + ss) / 60
+
+# Add converted minutes to degrees.
+dd = int(x[:2]) + mm
+
+# Print the final result with a message.
+print('Your coordinate in decimal degrees is {0}'.format(str(round(dd,2))))
